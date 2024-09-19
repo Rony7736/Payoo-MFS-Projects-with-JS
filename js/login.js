9184e534c7fdf18d2ce51f6c1c7699530094c4f2
Rony7736 <ronychakrabarty99@gmail.com>
2024-09-19 11:38:06 +0600

    get number in console

--- js/login.js
+++ js/login.js
@@ -1,4 +1,19 @@
 
-console.log('rony');
+// console.log('rony');
+
+// search: form submit reloading the page
+
+// step -1 ---> set event handler
+document.getElementById('btn-login').addEventListener('click', function(event){
+
+    //step - 2 --> prevent default behavior (prevent realoding browser)
+    event.preventDefault();         // jodi amra form tag use kori tahole eita dite hoy page reload na houar jonno
+
+    // step -3 --> Get the phone number
+    const phoneNumber = document.getElementById('phone-number').value
+    console.log(phoneNumber);
+    
+    
+})
 
 
