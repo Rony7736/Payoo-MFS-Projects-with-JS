c44c43028fd4f80ce6f637780055b45779da0dda
Rony7736 <ronychakrabarty99@gmail.com>
2024-09-19 17:47:07 +0600

    added the money event and add money and increase the current balance

--- js/home.js
+++ js/home.js
@@ -8,12 +8,37 @@ document.getElementById('btn-add-money').addEventListener('click', function(even
     // prevent page reload after form submit
     event.preventDefault();
 
-    // get money to be added to the account
+    // step : 2 --> get money to be added to the account
     const addMoneyInput = document.getElementById('add-money-input').value
 
     // get the pin number provided
     const pinInput = document.getElementById('pin-input').value
     
     console.log(addMoneyInput, pinInput);
+
+    // step : 3 --> varify pin number
+    if(pinInput === '1234'){
+        console.log('Adding money to your Account');
+
+        // step : 4 --> get the current Balance
+        const balance = document.getElementById('account-balance').innerText
+            
+        // step : 5 --> add moneyInput with balance
+        const addMoneyNumber = Number(addMoneyInput)
+        const balanceNumber = Number(balance)
+        
+        const newBalance = addMoneyNumber + balanceNumber
+        console.log(newBalance);
+
+        // step : 6 --> update the balance in the UI/DOM
+
+        document.getElementById('account-balance').innerText = newBalance
+        
+        
+
+    }
+    else{
+        alert('Failed to add money! Please input the right Pin ')
+    }
     
 })
