f0b3c0041e00187874911f2e3718ff47b00ebf44
Rony7736 <ronychakrabarty99@gmail.com>
2024-09-19 12:09:43 +0600

    successful login and click login and go to another html file

--- js/login.js
+++ js/login.js
@@ -9,12 +9,19 @@ document.getElementById('btn-login').addEventListener('click', function(event){
     //step - 2 --> prevent default behavior (prevent realoding browser)
     event.preventDefault();         // jodi amra form tag use kori tahole eita dite hoy page reload na houar jonno
 
-    // step -3 --> Get the phone number
+    // step -3 --> Get the phone number and the pin number
     const phoneNumber = document.getElementById('phone-number').value
     const pinNumber = document.getElementById('pin-number').value
 
-    console.log(phoneNumber);
-    console.log(pinNumber);
+    // Step -4 --> valid phone and pin
+    if(phoneNumber === '01749669919' && pinNumber === '1234'){
+        console.log('You are loged in');
+        window.location.href = './home.html'
+    }
+    else{
+        alert ('Wrong phone number or pin');
+        
+    }
     
     
 })
